bf4a5514975b390f65662344281947f5bf8cb4f8
Dennis Vita <vita3@uniba.sk>
2021-11-14 11:08:58 +0100

    Initial commit
    A basic program "card_reader.ino" that reads ISO14443A cards and
    displays the UID value in byte-code and decimal representation.
    The program uses SPI interface with a PN532 nfc reader module.

---
+++ src/card_reader/card_reader.ino
@@ -0,0 +1,61 @@
+
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include "PN532.h"
+
+PN532_SPI pn532spi(SPI, 16);
+PN532 nfc(pn532spi);
+
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Waiting for an ISO14443A Card ...");
+  Serial.println("");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+
+    //Convert UID Value into decimal representation and display it
+    uint64_t snr = 0;
+    for (size_t i=0; i<uidLength; ++i) {
+      uint64_t tmp = uid[i];
+      snr += tmp << (8*i); 
+    }
+    Serial.print("  SNR: ");
+    Serial.println(snr);
+    Serial.println("");
+    
+  }
+}
